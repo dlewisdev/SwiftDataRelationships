a25c12034a3cf005bdbcaca0de43377c46aa086b
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-19 22:58:00 -0500

    Initial Commit

---
+++ SwiftData Relationships/SwiftData_RelationshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_RelationshipsApp.swift
+//  SwiftData Relationships
+//
+//  Created by Danielle Lewis on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_RelationshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
